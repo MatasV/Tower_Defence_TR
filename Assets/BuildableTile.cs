43f72b70f5695b46ff8f955e2d72d3742437f5ee
MatasV <100126861+MatasVaidelauskas@users.noreply.github.com>
2024-03-27 16:49:39 +0200

    Added Tiles

---
+++ Assets/BuildableTile.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildableTile : MonoBehaviour
+{
+    [SerializeField] private Transform towerPlacementTransform;
+    [SerializeField] private bool isOccupied;
+    
+    
+}
